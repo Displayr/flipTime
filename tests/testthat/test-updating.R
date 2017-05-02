5b9a2dcbfc706cd031b48f902a35b3624a59f977
jakehoare <jake_hoare@hotmail.com>
2017-05-02 14:03:20 +1000

    testthat for UpdateAt and UpdateEvery

---
+++ tests/testthat/test-updating.R
@@ -0,0 +1,15 @@
+context("updating")
+
+test_that("updating", {
+    expect_equal(TimeUnitsToSeconds(30, units = "minutes"),  1800)
+    expect_equal(TimeUnitsToSeconds(12, units = "hours"),  43200)
+    expect_equal(TimeUnitsToSeconds(5, units = "days"),  432000)
+    expect_error(TimeUnitsToSeconds(3.5, units = "months"))
+    expect_message(UpdateEvery(60, units = "minutes"), message("R output expires in 3600 seconds with wakeup"))
+    expect_message(UpdateEvery(2, units = "weeks"), message("R output expires in 1209600 seconds with wakeup"))
+    expect_message(UpdateAt("15-05-2017 18:00:00", time.zone = "America/New_York", units = "days", frequency = 3))
+    expect_message(UpdateAt("1-12-2017 23:59:59", time.zone = "Europe/Rome", units = "weeks", frequency = 2))
+    expect_message(UpdateAt("31-12-2016 2:00:00", time.zone = "UTC", units = "months", frequency = 1))
+    expect_message(UpdateAt("1-3-2017 00:00:00", time.zone = "Africa/Nairobi", us.format = TRUE, units = "months", frequency = 12))
+    expect_message(UpdateAt("11-30-2016 4:55:00", time.zone = "Australia/Canberra", us.format = TRUE, units = "days", frequency = 14))
+})
