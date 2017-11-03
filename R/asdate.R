68cbb088df5268869919beb93ce06aa9c94c2bec
Mathew W. McLean <mathew.w.mclean@gmail.com>
2017-11-03 15:49:13 +1100

    Rename files to match function names
    * Split AsDateTime and AsDate code and tests into
    separate files and rename.

---
+++ R/asdate.R
@@ -0,0 +1,66 @@
+#' Parse Character Dates To POSIXct Objects
+#'
+#' Parse dates assuming some common (unknown) formats
+#' popular either in the U.S. or internationally
+#' @param x character; vector to be parsed
+#' @param us.format logical; whether to use the US convention for dates; can be \code{NULL}
+#' in which case both U.S. formats and international formats will be checked
+#' @param exact see \code{\link[lubridate]{parse_date_time2}}
+#' @return a vector of POSIXct date-time objects
+#' @examples
+#' AsDate("1-2-2017", us.format = FALSE)
+#' @importFrom lubridate parse_date_time2
+#' @export
+AsDate <- function(x, us.format = NULL, exact = TRUE)
+{
+    if (any(c("POSIXct", "POSIXt") %in% class(x)))
+        return(x)
+
+    ## The order of orders has been carefully selected.
+    ## Ensure that unit tests still pass if the order is changed.
+    ## mY and Ym should be before ymd, dmy, etc.
+    ## since sparse_date_time("10-10-10", "mY", exact = TRUE) fails
+    orders <- c("Ybd", "dby", "dbY", "bY", "by", "bdy", "bdY", "Yb", "yb", "Ymd", "mY", "Ym")
+    orders <- if (is.null(us.format))
+                        c(orders, "mdY", "mdy", "dmY", "dmy")
+                    else if (us.format)
+                        c(orders, "mdY", "mdy")
+                    else
+                        c(orders, "dmY", "dmy")
+    #orders <- c(orders, c("my", "Ymd", "y", "Y"))
+    orders <- c(orders, c("ymd", "ybd", "ydm", "Y", "my", "ym"))
+
+    x1 <- x[1L]
+
+    ## Try 'bY' and 'by' formats
+    ## need to handle this case separately as lubridate <= 1.6.0
+    ## fails to parse them
+    sep <- checkbYformat(x1)
+    if (!is.na(sep))
+    {
+        x <- sub("^([[:alpha:]])", "\\U\\1", x, perl = TRUE)  # lubridate fails if month not capitalized
+        if (grepl("[0-9]{4}$", x1))
+            return(parse_date_time2(paste("01", x, sep = sep), orders = "dbY", exact = exact))
+        else
+            return(parse_date_time2(paste("01", x, sep = sep), orders = "dby", exact = exact))
+    }
+
+    for (ord in orders)
+    {
+        parsed <- parse_date_time2(x1, ord, exact = exact)
+        if (!is.na(parsed))
+        {
+            parsed <- checkUSformatAndParse(x, ord,
+                                            unknown.format = is.null(us.format), exact = exact)
+            ## could have false positive match on first elem. e.g. mdY matches
+            ## even though it's clear from later elem. that dmY is correct
+            if (all(!is.na(parsed)))
+                break
+        }
+    }
+    if (is.na(parsed[1L]))
+        return(rep.int(NA, length(x)))
+
+    ## result <- parse_date_time2(x, ord, exact = TRUE)
+    parsed  # result
+}
