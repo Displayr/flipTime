26e54c1e71229df53cb12871cf3239c18c2415b2
TimBock <tim.bock@numbers.net.au>
2018-02-09 14:34:14 +1100

    DS-1890 Avoiding Lubridate bug with date differences

--- R/maths.R
+++ R/maths.R
@@ -4,7 +4,8 @@
 #' @param from The earlier date.
 #' @param to The later date.
 #' @param by The period used in the conversion. Either "month" or "year".
-#' @param ceiling If TRUE, rounds partial-year differences up (otherwise they are rounded down).
+#' @param ceiling If TRUE, rounds partial-year differences up (otherwise they are rounded
+#' down). Ignores seconds, minutes, and hours.
 #' @return An integer.
 #' @details Dates of 29th of Feb are automatically converted to "2016-02-28" to deal with bugs in lubridate.
 #' @examples
@@ -13,25 +14,18 @@
 #' @export
 DiffPeriod <- function(from, to, by, ceiling = FALSE)
 {
-initial.from = from #Delete after fixing bug
-initial.to = to  #Delete after fixing bug
     n <- length(from)
     n.to <- length(to)
     if (n != n.to)
         stop("'from' and 'to' have different lengths.")
     from <- Change29FebTo28th(as.Date(AsDateTime(from)))
     to <- Change29FebTo28th(as.Date(AsDateTime(to)))
-    diff <- interval(from, to) %/% switch(by, month = months(1), year = years(1))
-    remainder <- as.period(interval(from, to) %% switch(by,  month = months(1), year = years(1)))
-    if (ceiling)
+    if (by == "day")
+        diff <- as.numeric(to - from)
+    else
     {
-        d <- remainder@month + remainder@day + remainder@hour + remainder@minute > 0
-        if (any(is.na(d)))
-        {
-            print(initial.from[is.na(d)])
-            print(initial.to[is.na(d)])
-        }
-        diff[d] <- diff[d] + 1
+        diff <- DecimalDate(to, by) - DecimalDate(from, by)
+        diff <- if (ceiling) ceiling(diff) else (floor(diff))
     }
     diff
 }
@@ -51,3 +45,37 @@ Change29FebTo28th <- function(x)
     x
 }
 
+
+
+
+#' DecimalDate
+#'
+#' @details Turns a date into a integer, relative to an origin of 1900-01-01. Ignores hours, minutes and seconds.
+#' @param x Date
+#' @param by The period used in the conversion. Either "month" or "year".
+#' @importFrom lubridate days_in_month leap_year
+#' @examples
+#' DecimalDate("2000/01/01", by = "year")
+#' DecimalDate("2000/01/01", by = "month")
+#' DecimalDate("2000/02/01", by = "month")
+#' DecimalDate("2000/06/16", by = "month")
+#' @export
+DecimalDate <- function(x, by) {
+
+    date <- as.Date(x, origin="1900/01/01")
+    d <- as.POSIXlt(date);
+    month <- d$mon
+    year <- d$year
+    if (by == "month")
+    {
+        month.day <- d$mday
+        days.in.month <- days_in_month(date)
+        return(as.numeric(year * 12 + month + (month.day - 1)/days.in.month))
+    } else if (by == "year")
+    {
+    days.in.year <- ifelse(leap_year(date), 366, 365)
+    day.in.year <- d$yday
+    return(year + day.in.year/ days.in.year)
+    }
+    stop("Unknown 'by'.")
+}
--- tests/testthat/test-maths.R
+++ tests/testthat/test-maths.R
@@ -26,10 +26,27 @@ test_that("DiffPeriod", {
     # Vectorized - month
     expect_equal(DiffPeriod(rep("2015/05/06", 3), c("2017/05/06", "2017/05/05", "2017/05/07"), by = "month", ceiling = FALSE), c(24, 23, 24))
     expect_equal(DiffPeriod(rep("2015/05/06", 3), c("2017/05/06", "2017/05/05", "2017/05/07"), by = "month", ceiling = TRUE), c(24, 24, 25))
+    # Checking for error
+a = c("2016-04-29 12:00:00 GMT", "2013-10-31 12:00:00 GMT", "2012-05-31 12:00:00 GMT", "2010-06-29 12:00:00 GMT", "2014-12-31 12:00:00 GMT", "2015-08-31 12:00:00 GMT", "2013-03-29 12:00:00 GMT", "2014-07-31 12:00:00 GMT")
+b = c("2017-03-25", "2014-03-16", "2012-12-15", "2011-03-25", "2015-10-16" ,"2016-03-16", "2014-03-28" ,"2015-07-22")
+expect_error(DiffPeriod(a, b, ceiling = TRUE, by = "month"), NA)
+
 })
 
 
 test_that("Change29FebTo28th", {
     # Scalars - year
     expect_equal(Change29FebTo28th(as.Date(c("2016/02/29", "2016/02/28"))),as.Date(c("2016/02/28", "2016/02/28")))
+})
+
+test_that("NumericDatedates", {
+    expect_equal(DecimalDate("2000/01/01", by = "year"), 100)
+    expect_equal(DecimalDate("2000/01/01", by = "month"), 1200)
+    expect_equal(DecimalDate("2000/02/01", by = "month"), 1201)
+    expect_equal(DecimalDate("2000/06/16", by = "month"), 1205.5)
+    expect_equal(DecimalDate("2000/02/01", by = "month") - DecimalDate("2000/01/01", by = "month"), 1)
+    expect_equal(DecimalDate("2000/03/01", by = "month") - DecimalDate("2000/01/01", by = "month"), 2)
+    d1 <- DecimalDate("2000/03/01", by = "year") - DecimalDate("2000/01/01", by = "year")
+    d2 <- DecimalDate("2001/03/01", by = "year") - DecimalDate("2001/01/01", by = "year")
+    expect_true(d2 < d1)
 })
